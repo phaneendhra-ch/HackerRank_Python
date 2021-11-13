1ba8ec902a186adcb776d4eefff1379a39a7b5f5
Phaneendhra <phani4842@gmail.com>
2021-11-13 18:55:07 +0530

    Regex Substitution

---
+++ RegexSubstitute.py
@@ -0,0 +1,28 @@
+"""
+
+Here Substitute the string
+
+    -> ' && ' with ' and '
+    -> ' || ' with ' or '
+
+Author : Phaneendhra
+
+"""
+
+import re
+
+N = int(input())
+str_ = ""
+for each in range(N):
+    c = input()
+    while ((' && ' in c) or (' || ' in c)):         # iterate while for one or more occurences in same line
+
+        c = re.sub(' \&\& ',' and ',c)              # Substitute && with and,
+                                                      # if string is &&& then it remains same
+
+        c = re.sub(' \|\| ',' or ',c)               # Substitute || with or,
+                                                      # if string is ||| then it remains same
+
+    str_+=c+'\n'                                    # append the updated string
+
+print(str_)                                         # print the string after replacing with 'and' and 'or' 
