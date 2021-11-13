a5ace756cb25027639c8651c5d2e8c682a335b23
Phaneendhra <phani4842@gmail.com>
2021-11-13 19:00:22 +0530

    Regex Substitution

--- RegexSubstitute.py
+++ RegexSubstitute.py
@@ -15,14 +15,14 @@ N = int(input())
 str_ = ""
 for each in range(N):
     c = input()
-    while ((' && ' in c) or (' || ' in c)):         # iterate while for one or more occurences in same line
-
+    while (' && ' in c):                    # iterate while for one or more occurences in same line
         c = re.sub(' \&\& ',' and ',c)              # Substitute && with and,
-                                                      # if string is &&& then it remains same
+                                                    # if string is &&& then it remains same
 
+    while(' || ' in c):                     # if string is &&& then it remains same
         c = re.sub(' \|\| ',' or ',c)               # Substitute || with or,
-                                                      # if string is ||| then it remains same
+                                                    # if string is ||| then it remains same
 
     str_+=c+'\n'                                    # append the updated string
 
-print(str_)                                         # print the string after replacing with 'and' and 'or' 
+print(str_)                                         # print the string after replacing with 'and' and 'or'
